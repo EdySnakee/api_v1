d8a32b38c0b5126ffe5a4e9ab1b0804683630787
Edy <94276786+EdySnakee@users.noreply.github.com>
2025-06-20 12:56:17 -0600

    Usabdi sqlite

--- db.py
+++ db.py
@@ -1,10 +1,29 @@
-from typing import Annotated
+# from typing import Annotated
 
+# from fastapi import Depends, FastAPI
+# from sqlmodel import SQLModel, Session, create_engine
+
+# mysql_url = "mysql+mysqlconnector://root:@localhost/apiUno"
+# engine = create_engine(mysql_url, echo=True)
+
+# def create_all_tables(app: FastAPI):
+#     SQLModel.metadata.create_all(engine)
+#     yield
+
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
+        
+# SessionDep = Annotated[Session, Depends(get_session) ]
+
+
+# PARA SQLITE -->>
+from typing import Annotated
 from fastapi import Depends, FastAPI
 from sqlmodel import SQLModel, Session, create_engine
 
-mysql_url = "mysql+mysqlconnector://root:@localhost/apiUno"
-engine = create_engine(mysql_url, echo=True)
+sqlite_url = "sqlite:///./db.sqlite3"
+engine = create_engine(sqlite_url, echo=True)
 
 def create_all_tables(app: FastAPI):
     SQLModel.metadata.create_all(engine)
@@ -13,5 +32,5 @@ def create_all_tables(app: FastAPI):
 def get_session():
     with Session(engine) as session:
         yield session
-        
-SessionDep = Annotated[Session, Depends(get_session) ]
+
+SessionDep = Annotated[Session, Depends(get_session)]
